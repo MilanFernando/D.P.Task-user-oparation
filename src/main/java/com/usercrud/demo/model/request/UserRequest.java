e2856faa5b0531e23c25674e6e1f47016d1027a4
Milan D Fernando <milandilshan5000@gmail.com>
2025-03-31 16:32:15 +0530

    done by create annotation for validate nic and phone number

--- src/main/java/com/usercrud/demo/model/request/UserRequest.java
+++ src/main/java/com/usercrud/demo/model/request/UserRequest.java
@@ -1,5 +1,7 @@
 package com.usercrud.demo.model.request;
 
+import com.usercrud.demo.anotation.ValidateNic;
+import com.usercrud.demo.anotation.ValidatePhone;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,4 +13,9 @@ public class UserRequest {
     private String username;
     @NotEmpty(message = "password is required")
     private String password;
+    @ValidatePhone(message="Phone number is required")
+    private String phoneNumber;
+    @ValidateNic(message = "Nic is required")
+    private String nic;
+
 }
